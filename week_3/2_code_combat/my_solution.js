80ef417ddc2a8eb89689aff1976b73bb202a1ff2
MarekT <rkx1901@gmail.com>
2014-05-11 08:45:06 -0500

    Added pseudocode

--- week_3/2_code_combat/my_solution.js
+++ week_3/2_code_combat/my_solution.js
@@ -1,16 +1,98 @@
-// I worked on this challenge [by myself, with:]
- 
-// For each mission, write the title as a comment. (Shown here). Also include pseudocode as a comment. 
-// Note: to make commenting easier, you can highlight the section you want to comment and hold 
-// command + / This will comment the line. 
- 
- 
- 
- 
- 
- 
- 
- 
- 
+// I worked on this challenge [by myself]
+
+// For each mission, write the title as a comment. (Shown here). Also include pseudocode as a comment.
+// Note: to make commenting easier, you can highlight the section you want to comment and hold
+// command + / This will comment the line.
+
+ //Rescue Mission
+ //
+ // move down(2)
+ // move right
+ // move up(2)
+ // move right(2)
+ // move down
+ // attack
+
+// Greab the Mushroom
+//
+// move up
+// move right
+// move left
+// move up
+// attack
+
+// Drink me
+//
+// attack
+// move right
+// move down
+// move up
+// move right
+// attack
+
+
+// Taunt the guards
+//
+// move right(2)
+// move up
+// move right
+// say hey there
+// say attack(2)
+// move right
+// say follow me
+// move right(2)
+
+
+// It's a trap
+
+// move down(2)
+// say hey there
+// move up(2)
+
+
+// Break th eprison
+
+// if name = Wiiliam || name = Lucas || name = "Marcus" || name = Robert
+//   return true
+// else
+//   return false
+
+
+
+//  Taunt
+
+
+// say "ugly"
+// say "you are very ugly"
+// say "you just ugly"
+// say "i can't look at you"
+
+
+//Cowardly taunt
+
+// move close to troll
+// say "hey troll"
+// go back to safe spot
+
+// Commanding Followers
+
+// say "follow me"
+// move forward and stop half way
+// say "fight"
+// go back to safe spot
+
+// Mobile Artillery
+
+// move artillerty to target trolls on north
+// shoot(2)
+// move artillerty to target trolls on east
+// shoot(2)
+
+
+
+
 // Reflection:
 // Write your reflection here.
+ // I have to say, that I didn't not enjoy this assignment at all.
+ // It took me a lot of time to complete these levels, mostly because site was slow and I think sometimes buggy.
+ // I had to refresh web pages several times before it let me pass.
